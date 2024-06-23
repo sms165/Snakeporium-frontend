9ad70f88ad04cf2be0b1d61832d39eded2c8ca96
sms165 <61697688+sms165@users.noreply.github.com>
2024-06-23 20:37:33 +0200

    ng build

--- server.ts
+++ server.ts
@@ -4,7 +4,7 @@ import express from 'express';
 import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
 import AppServerModule from './src/main.server';
-
+import compression from 'compression';
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
   const server = express();
@@ -16,6 +16,8 @@ export function app(): express.Express {
 
   server.set('view engine', 'html');
   server.set('views', browserDistFolder);
+
+
 
   // Example Express Rest API endpoints
   // server.get('/api/**', (req, res) => { });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ApplicationRef, Component } from '@angular/core';
 import { UserStorageService } from './services/storage/user-storage.service';
 import { Router } from '@angular/router';
 
@@ -13,13 +13,24 @@ export class AppComponent {
   isCustomerLoggedIn : boolean = UserStorageService.isCustomerLoggedIn();
   isAdminLoggedIn : boolean = UserStorageService.isAdminLoggedIn();
 
-  constructor (private router: Router){}
+  constructor (private router: Router,
+    private appRef: ApplicationRef
+  ){}
 
   ngOnInit(): void {
     this.router.events.subscribe(event => {
       this.isCustomerLoggedIn = UserStorageService.isCustomerLoggedIn();
       this.isAdminLoggedIn = UserStorageService.isAdminLoggedIn();
     })
+    this.appRef.isStable.subscribe((isStable: boolean) => {
+      if (isStable) {
+        console.log('Application is stable');
+        // Perform actions once the application is stable
+      } else {
+        console.log('Application is not yet stable');
+      }
+    });
+
   }
 
   logout(){
