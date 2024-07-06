fae0cbf9fff1e5de51dce3a9b4f352e38c0b07d2
sms165 <61697688+sms165@users.noreply.github.com>
2024-07-06 19:20:49 +0200

    responsive navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { ApplicationRef, Component } from '@angular/core';
+import { ApplicationRef, Component, HostListener } from '@angular/core';
 import { UserStorageService } from './services/storage/user-storage.service';
 import { Router } from '@angular/router';
 
@@ -13,11 +13,14 @@ export class AppComponent {
   isCustomerLoggedIn : boolean = UserStorageService.isCustomerLoggedIn();
   isAdminLoggedIn : boolean = UserStorageService.isAdminLoggedIn();
 
+  isSmallScreen: boolean;
+
   constructor (private router: Router,
     private appRef: ApplicationRef
   ){}
 
   ngOnInit(): void {
+    this.checkScreenSize();
     this.router.events.subscribe(event => {
       this.isCustomerLoggedIn = UserStorageService.isCustomerLoggedIn();
       this.isAdminLoggedIn = UserStorageService.isAdminLoggedIn();
@@ -26,9 +29,20 @@ export class AppComponent {
 
   }
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.checkScreenSize();
+  }
+
+  checkScreenSize() {
+    this.isSmallScreen = window.innerWidth <= 768; // Adjust the width as needed
+  }
+
   logout(){
     UserStorageService.signOut();
     this.router.navigateByUrl("login");
   }
+
+
 
 }
